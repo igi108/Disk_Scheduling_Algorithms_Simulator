4a099ee7f994a9b682669ff0e89619314460ec79
igi108 <igorfilipowski@gmail.com>
2026-04-18 23:16:54 +0200

    feat(core) change Request update function and fields

--- src/Request.java
+++ src/Request.java
@@ -2,24 +2,28 @@ public class Request {
 
     public int id;
     public int position;//id of place where request sits
+    public boolean finished;
 
     //time is counted since beginning of simulation
     public int arrivalTime;//time of request creation
-    public int finishTime;
+    public int finishTime;//time of request finish. null means request can still be finished
     public int deadline;//moment of time until which request has to be done
 
     public Request(){
-        this.finishTime = -1;//request not finished
         this.deadline = -1;//basic request has no deadline
+        this.finished = false;
     }
 
     public void update(int currentTime, int headPosition){
 
         if(headPosition == position){
             finishTime = currentTime;
+            finished = true;
+            return;
         }
-        if(currentTime > deadline){
+        if(deadline != -1 && currentTime > deadline){
             finishTime = -1;
+            finished = false;
         }
 
     }
