77be3875a553e017949a56548545cd831304de69
igi108 <igorfilipowski@gmail.com>
2026-09-22 11:23:32 +0200

    feat(core) change main for easier understanding -added values for normal test -added printed message

--- src/Main.java
+++ src/Main.java
@@ -60,23 +60,31 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         //test normal system usage
+        diskSize = 2000;
+        totalRequests = 3000;
+        totalArrivalTime = 20000;
+        clutterPercentage = 0.2;
+        numberOfClutters = 6;
+        maxClutterDistance = 80;
+        deadlinesPercentage = 0.08;
+        minDeadline = 20;
+        maxDeadline = 500;
         testAllAlgorithms();
+        System.out.println("\nPress ENTER to continue...");
 
         //test random chaos
         if(scanner.hasNextLine()){
             diskSize = 2000;
             totalRequests = 6000;
             totalArrivalTime = 50000;
-
             clutterPercentage = 0.02;
             numberOfClutters = 1;
             maxClutterDistance = 200;
-
             deadlinesPercentage = 0.02;
             minDeadline = 10;
             maxDeadline = 50;
-
             testAllAlgorithms();
+            System.out.println("\nPress ENTER to continue...");
             scanner.nextLine();
         }
 
@@ -85,15 +93,12 @@ public class Main {
             diskSize = 2000;
             totalRequests = 3000;
             totalArrivalTime = 15000;
-
             clutterPercentage = 0.60;
             numberOfClutters = 4;
             maxClutterDistance = 40;
-
             deadlinesPercentage = 0.05;
             minDeadline = 50;
             maxDeadline = 300;
-
             testAllAlgorithms();
         }
 
